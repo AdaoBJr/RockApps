5e9cf3fa1367556ff6340fc5fdceb7ca83643e04
Adão Benites <adaobjr@gmail.com>
2021-08-30 01:22:52 -0400

    feat: ✨ updated helpers functions

--- src/functions/index.js
+++ src/functions/index.js
@@ -26,3 +26,54 @@ export const removeItem = (id, arrayData) => {
 };
 
 // ----------------------------------------------------------------------------------------------
+
+// LOGIN
+
+export const AddToUsers = (register, users, Email) => {
+  const { RuserName: userName, Remail: email, Rpassword: password } = register;
+
+  // DEFINE ALL USERS ACTIVE: FALSE
+  const Users = [...users];
+  for (let i = 0; i < Users.length; i += 1) {
+    Users[i].active = false;
+  }
+
+  if (register || Email) {
+  // FIND USER
+    const findUser = Users.find((item) => item.email === Email);
+
+    // ADD NEW USER
+    if (!Users.length || !findUser) {
+      const newUsers = [...users, {
+        userName, email, password, active: true,
+      }];
+      setStorage('LSusers', newUsers);
+      return newUsers;
+    }
+
+    // UPDATE USER
+    const key = Users.indexOf(findUser);
+    Users[key].active = true;
+    if (register) {
+      Users[key].userName = userName;
+      Users[key].password = password;
+    }
+  }
+  setStorage('LSusers', Users);
+  return Users;
+};
+
+export const AddPhotoToUsers = (users, photo) => {
+  const Users = [...users];
+  // FIND USER ACTIVE
+  const findUser = Users.find((item) => item.active);
+
+  // ADD PHOTO
+  const key = Users.indexOf(findUser);
+  Users[key].photo = photo;
+
+  setStorage('LSusers', Users);
+  return Users;
+};
+
+// ----------------------------------------------------------------------------------------------
