67f6204bf7c2193221bd3fa99f8d1b59046589ce
Adão Benites <adaobjr@gmail.com>
2021-08-30 00:35:31 -0400

    feat: ✨ added prop-types in Header

--- src/common/components/Header.jsx
+++ src/common/components/Header.jsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default function Header() {
+export default function Header({ colec }) {
+  const dispatch = useDispatch();
+  const {
+    screen: {
+      home, fav, carT, profile, lightTheme,
+    },
+    cart: { cart },
+    user: { logIn },
+  } = useSelector((state) => state);
   return (
     <div />
   );
 }
+
+Header.propTypes = {
+  colec: PropTypes.bool,
+};
+
+Header.defaultProps = {
+  colec: undefined,
+};
