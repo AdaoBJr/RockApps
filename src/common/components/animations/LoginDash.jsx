a857cb2fd9f756067114ca981a2539971684833a
Adão Benites <adaobjr@gmail.com>
2021-08-30 01:06:58 -0400

    feat: ✨ added content LoginDash

--- src/common/components/animations/LoginDash.jsx
+++ src/common/components/animations/LoginDash.jsx
@@ -1,7 +1,51 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import Lottie from 'react-lottie';
 
-export default function LoginDash() {
+import * as GoLoginDashboard from '../../../GoLoginDash.json';
+import * as GoLogoutDashboard from '../../../GoLogoutDash.json';
+
+const LoginDashboard = {
+  loop: true,
+  autoplay: true,
+  animationData: GoLoginDashboard.default,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice',
+  },
+};
+
+const LogoutDashboard = {
+  loop: true,
+  autoplay: true,
+  animationData: GoLogoutDashboard.default,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice',
+  },
+};
+
+export default function LoginDash({ page }) {
+  if (page === 'logout') {
+    return (
+      <div className="goLoginDashboard">
+        <Lottie
+          options={LogoutDashboard}
+        />
+      </div>
+    );
+  }
   return (
-    <div />
+    <div className={(page === 'login') ? 'loginDashboard' : 'goLoginDashboard'}>
+      <Lottie
+        options={LoginDashboard}
+      />
+    </div>
   );
 }
+
+LoginDash.propTypes = {
+  page: PropTypes.string,
+};
+
+LoginDash.defaultProps = {
+  page: undefined,
+};
