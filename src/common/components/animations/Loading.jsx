2d9880e2c64f61d5c26923b4621c5ca8e6347c50
Adão Benites <adaobjr@gmail.com>
2021-08-30 01:54:22 -0400

    feat: ✨ added content Loading

--- src/common/components/animations/Loading.jsx
+++ src/common/components/animations/Loading.jsx
@@ -1,7 +1,52 @@
 import React from 'react';
+import Lottie from 'react-lottie';
+import PropTypes from 'prop-types';
 
-export default function Loading() {
+import * as loadingLottie from '../../../loading.json';
+import * as doneLottie from '../../../done.json';
+
+const loadingAnimation = {
+  loop: true,
+  autoplay: true,
+  animationData: loadingLottie.default,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice',
+  },
+};
+
+const doneAnimation = {
+  loop: false,
+  autoplay: true,
+  animationData: doneLottie.default,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice',
+  },
+};
+
+export default function Loading({ loading }) {
   return (
-    <div />
+    <div className="bgLoading">
+      {!loading ? (
+        <Lottie
+          options={loadingAnimation}
+          height={420}
+          width={420}
+        />
+      ) : (
+        <Lottie
+          options={doneAnimation}
+          height={220}
+          width={220}
+        />
+      )}
+    </div>
   );
 }
+
+Loading.propTypes = {
+  loading: PropTypes.bool,
+};
+
+Loading.defaultProps = {
+  loading: undefined,
+};
