d9209dd3164de4dd1ab22fbdb616d85bb4a3498b
Adão Benites <adaobjr@gmail.com>
2021-08-30 02:41:02 -0400

    feat: ✨ added content LoadingPay

--- src/common/components/animations/LoadingPay.jsx
+++ src/common/components/animations/LoadingPay.jsx
@@ -1,7 +1,52 @@
 import React from 'react';
+import Lottie from 'react-lottie';
+import PropTypes from 'prop-types';
 
-export default function LoadingPay() {
+import * as loadingPayLottie from '../../../loadingPay.json';
+import * as donePayLottie from '../../../donePay.json';
+
+const loadingPayAnimation = {
+  loop: true,
+  autoplay: true,
+  animationData: loadingPayLottie.default,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice',
+  },
+};
+
+const donePayAnimation = {
+  loop: false,
+  autoplay: true,
+  animationData: donePayLottie.default,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice',
+  },
+};
+
+export default function LoadingPay({ loading }) {
   return (
-    <div />
+    <div className="bgLoading">
+      {!loading ? (
+        <Lottie
+          options={loadingPayAnimation}
+          height={420}
+          width={420}
+        />
+      ) : (
+        <Lottie
+          options={donePayAnimation}
+          height={220}
+          width={220}
+        />
+      )}
+    </div>
   );
 }
+
+LoadingPay.propTypes = {
+  loading: PropTypes.bool,
+};
+
+LoadingPay.defaultProps = {
+  loading: undefined,
+};
