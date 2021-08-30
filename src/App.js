aa30bf52af37913ea45d4e539899a5b1ebc3dc36
Adão Benites <adaobjr@gmail.com>
2021-08-29 23:47:59 -0400

    feat: ✨ created Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import React from 'react';
+import {
+  Switch, Route,
+} from 'react-router-dom';
+
+import Cart from './common/pages/Cart';
+import Checkout from './common/pages/Checkout';
+import Favorited from './common/pages/Favorited';
+import Home from './common/pages/Home';
+import Profile from './common/pages/Profile';
+import Purchased from './common/pages/Purchased';
+import ShoesDetails from './common/pages/ShoesDetails';
 
 function App() {
   return (
-    <div>
-      Removed files not used
-    </div>
+    <Switch>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/profile" component={Profile} />
+      <Route exact path="/favorited" component={Favorited} />
+      <Route exact path="/details/:id" component={ShoesDetails} />
+      <Route exact path="/cart" component={Cart} />
+      <Route exact path="/checkout" component={Checkout} />
+      <Route exact path="/purchased" component={Purchased} />
+    </Switch>
   );
 }
 
