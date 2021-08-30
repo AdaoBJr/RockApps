a18fdfc61e6bd07efff46f73da3387e8971b6ee0
Adão Benites <adaobjr@gmail.com>
2021-08-30 02:39:04 -0400

    feat: ✨ added content DashboardPay

--- src/common/components/animations/DashboardPay.jsx
+++ src/common/components/animations/DashboardPay.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import Lottie from 'react-lottie';
+
+import * as dashboardPayLottie from '../../../dashboardPay.json';
+
+const dashboardPayAnimation = {
+  loop: true,
+  autoplay: true,
+  animationData: dashboardPayLottie.default,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice',
+  },
+};
 
 export default function DashboardPay() {
   return (
-    <div />
+    <div className="checkoutDashboard">
+      <Lottie
+        options={dashboardPayAnimation}
+      />
+    </div>
   );
 }
