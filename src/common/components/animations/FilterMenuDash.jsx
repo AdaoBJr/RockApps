b51bed43009be2bef2b3c276212aef54b30cb9a0
Adão Benites <adaobjr@gmail.com>
2021-08-30 01:52:28 -0400

    feat: ✨ added content FilterMenuDash

--- src/common/components/animations/FilterMenuDash.jsx
+++ src/common/components/animations/FilterMenuDash.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import Lottie from 'react-lottie';
+
+import * as FilterMenuDashboard from '../../../filterMenu.json';
+
+const FilterDashboard = {
+  loop: true,
+  autoplay: true,
+  animationData: FilterMenuDashboard.default,
+  rendererSettings: {
+    preserveAspectRatio: 'xMidYMid slice',
+  },
+};
 
 export default function FilterMenuDash() {
   return (
-    <div />
+    <div className="filterMenuDash">
+      <Lottie
+        options={FilterDashboard}
+      />
+    </div>
   );
 }
