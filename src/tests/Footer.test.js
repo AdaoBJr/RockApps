85953f43fce7893eafb38e8bedc2174ce5134d8b
Adão Benites <adaobjr@gmail.com>
2021-08-30 00:51:12 -0400

    feat: :white_check_mark: updated tests in Footer Component

--- src/tests/Footer.test.js
+++ src/tests/Footer.test.js
@@ -12,4 +12,29 @@ describe('Testando toda a aplicação do Componente Footer', () => {
     const title = screen.getByText(/GoShoes/i);
     expect(title).toBeInTheDocument();
   });
+
+  it('Verifica se a frase "Envie um presente e proporcione alegria" aparece no Footer', () => {
+    renderWithRouter(<Footer />);
+
+    const phrase = screen.getByText(/Envie um presente e proporcione alegria/i);
+    expect(phrase).toBeInTheDocument();
+  });
+
+  it('Verifica se os links aparecem no Footer', () => {
+    renderWithRouter(<Footer />);
+
+    const Budget = screen.getByText(/Orçamento/i);
+    const Discount = screen.getByText(/Descontos/i);
+    const Ship = screen.getByText(/Frete/i);
+    const Blog = screen.getByText(/Blog/i);
+    const About = screen.getByText(/Sobre nós/i);
+    const OurMission = screen.getByText(/Nossa Missão/i);
+
+    expect(Budget).toBeInTheDocument();
+    expect(Discount).toBeInTheDocument();
+    expect(Ship).toBeInTheDocument();
+    expect(Blog).toBeInTheDocument();
+    expect(About).toBeInTheDocument();
+    expect(OurMission).toBeInTheDocument();
+  });
 });
