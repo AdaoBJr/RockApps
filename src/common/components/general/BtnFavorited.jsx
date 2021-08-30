973a18715e47a92e98909fc5e846ce1e378de9d8
Adão Benites <adaobjr@gmail.com>
2021-08-30 01:40:41 -0400

    feat: ✨ added content BtnFavorited

--- src/common/components/general/BtnFavorited.jsx
+++ src/common/components/general/BtnFavorited.jsx
@@ -1,7 +1,46 @@
 import React from 'react';
+import { useHistory } from 'react-router';
+import { useDispatch, useSelector } from 'react-redux';
 
-export default function BtnFavorited() {
+import { FaHeart, FaRegHeart } from 'react-icons/fa';
+import { setFav, setMsgLogin } from '../../../redux/actions';
+import { Fav } from '../../../functions';
+import { TIME_SEC } from '../../pages/Profile';
+
+export default function BtnFavorited({ product }) {
+  const history = useHistory();
+  const dispatch = useDispatch();
+
+  const {
+    products: { favorited },
+    user: { logIn },
+    screen: { details },
+  } = useSelector((state) => state);
+
+  const addToFav = () => {
+    if (logIn) {
+      dispatch(setFav(Fav(product, favorited)));
+      dispatch(setMsgLogin(false));
+    } else {
+      dispatch(setMsgLogin(true));
+      setTimeout(() => {
+        history.push('/profile');
+      }, TIME_SEC);
+    }
+  };
+
+  const renderBtnFavorited = () => (
+    <div
+      aria-hidden
+      className={(!details) ? (
+        'button favoritedButton') : ('button favoritedButtonDetails')}
+      onClick={() => addToFav()}
+    >
+      {(favorited.find((fav) => fav.id === product.id)) ? <FaHeart /> : <FaRegHeart /> }
+    </div>
+  );
+
   return (
-    <div />
+    renderBtnFavorited()
   );
 }
